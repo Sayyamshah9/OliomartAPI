6ecf0325e04a87e8bb1dedb4e73249bf7d60272f
Sayyam <sayyammshah@gmail.com>
2022-01-10 21:21:04 +0530

    Reset Password Second Varient from BranchOne

--- routers/user/password_reset.js
+++ routers/user/password_reset.js
@@ -1,39 +1,51 @@
-const pass_code_route = require('express').Router()
 const pass_reset_route = require('express').Router()
 const bcrypt = require('bcryptjs')
 const userschema = require('../../dbschemas/userschema')
+const password_validation = require('../../validations_and_auths/login_signup_validations')
 
+//Update Temprory Password in Database
+pass_reset_route.patch('/updatetemppass/:email/:temp_pass', async(req,res)=>{
+
+    const salt = await bcrypt.genSalt(11)
+    const hashed_new_pass = await bcrypt.hash(req.params.temp_pass, salt)
+
+    userschema.findOneAndUpdate({email:req.params.email}, 
+        {
+            password: hashed_new_pass,
+            confirmpassword: hashed_new_pass
+        },
+        (err) => {
+            if(err) return res.json({msg:err})
+            else{
+                res.json({msg:"New Password is Set"})
+            }
+        }
+    )
+})
+
+//PASSWORD RESET ROUTE
 pass_reset_route.patch('/:email', async(req,res)=>{
 
-    const userexist = await userschema.findOne({email:req.params.email})
-    if(userexist){
-
-        if(req.body.password == req.body.confirmpassword){
-
-        //hashing temp_pass
-        const salt = await bcrypt.genSalt(11)
-        const hashed_new_pass = await bcrypt.hash(req.body.password, salt)
-
-        //UPDATING NEW PASSWORD
-        userschema.findOneAndUpdate({email:req.params.email}, 
-            {
-                password: hashed_new_pass,
-                confirmpassword: hashed_new_pass
-            },
-            (err) => {
-                if(err) return res.json({msg:err})
-                else {
-                    res.json({msg:"Password Updated Successfully"})
-                }
-            })
-
-        }else{
-            res.json({msg:"Password didn't match"})
-        }
-        
-    }else{
-        res.json({msg:"User Does Not exist"})
+    if(req.body.password != req.body.confirmpassword){
+        return res.json({msg:"Password Didn't Match"})
     }
+
+    //hashing temp_pass
+    const salt = await bcrypt.genSalt(11)
+    const hashed_new_pass = await bcrypt.hash(req.body.password, salt)
+
+    userschema.findOneAndUpdate({email:req.params.email}, 
+        {
+            password: hashed_new_pass,
+            confirmpassword: hashed_new_pass
+        },
+        (err) => {
+            if(err) return res.json({msg:err})
+            else{
+                res.json({msg:"New Password is Set"})
+            }
+        }
+    )
 })
 
 module.exports = pass_reset_route
