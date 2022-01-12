7bcc7f8706ca10ea81d8b492f29685ef5aa41b5e
Sayyam <sayyammshah@gmail.com>
2022-01-12 09:43:00 +0530

    ocr error Solved

--- getText.js
+++ getText.js
@@ -2,20 +2,15 @@ const { createWorker } = require("tesseract.js");
 
 const worker = createWorker()
 
-// var date=new Date
-// console.log(date.toLocaleTimeString())
-
 module.exports = async function getText(fileName, adharName, adharNumber) {
-
-    var cnt = ""
     
     await worker.load()
     await worker.loadLanguage('eng')
     await worker.initialize('eng')
 
     const{data:{text}} = await worker.recognize(fileName)
-    await worker.terminate()
-    // console.log(text)
+    // await worker.terminate()
+
     var date=new Date
     console.log(date.toLocaleTimeString())
     
