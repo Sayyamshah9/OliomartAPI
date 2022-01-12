f5a702121dfd3446b8e0652526bf9722d715ae17
Sayyam <sayyammshah@gmail.com>
2022-01-12 09:58:06 +0530

    commit 1

--- getText.js
+++ getText.js
@@ -2,16 +2,7 @@ const { createWorker } = require("tesseract.js");
 
 const worker = createWorker()
 
-<<<<<<< HEAD
-// var date=new Date
-// console.log(date.toLocaleTimeString())
-
-module.exports = async function getText(req, res, fileName, adharName, adharNumber) {
-
-    var cnt = ""
-=======
 module.exports = async function getText(fileName, adharName, adharNumber) {
->>>>>>> branchOne
     
     await worker.load()
     await worker.loadLanguage('eng')
@@ -19,11 +10,6 @@ module.exports = async function getText(fileName, adharName, adharNumber) {
 
     const{data:{text}} = await worker.recognize(fileName)
     // await worker.terminate()
-<<<<<<< HEAD
-    // console.log(text)
-=======
-
->>>>>>> branchOne
     var date=new Date
     console.log(date.toLocaleTimeString())
     
