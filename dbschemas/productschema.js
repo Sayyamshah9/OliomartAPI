72532f6825f683281db3afabf6d605604ea14f64
Sayyam <sayyammshah@gmail.com>
2022-01-18 09:51:35 +0530

    Upload Products Route Added

---
+++ dbschemas/productschema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const userschema = require('./userschema')
+
+// const imgArray = new mongoose.Schema({
+//         pimg1: {type:String},
+//         pimg1: {type:String},
+//         pimg1: {type:String}
+// })
+
+const productschema = mongoose.Schema({
+
+    puserid: {type:String, ref:userschema},
+    pname: String,
+    pdescription: String,
+    pcategory: String,
+    psubcategory: String,
+    qty: Number,
+    price: Number,
+    pimages: [Object],
+    pweight: Number,
+    manufacturedate: String,
+    expirydate: String,
+    veg: Boolean,
+    pcolor: [String],
+    psize: [String],
+    pkeywords: [String],
+    pverify: Boolean
+
+})
+
+module.exports = mongoose.model('productdata', productschema)
