6cb7a37109cc5a983a9e876cd0ea28f6a9f27a36
Sayyam <sayyammshah@gmail.com>
2022-01-25 18:42:11 +0530

    Commit

--- routers/user/send_email_otp.js
+++ routers/user/send_email_otp.js
@@ -8,7 +8,7 @@ send_otp_router.put('/:email', async(req,res) => {
     const findUser = await userSchema.findOne({email:req.params.email})
     if(!findUser) return res.json({msg:"User Does not Exist"})
 
-    const otpGenerated = sendEmailOtp(req.params.email, "findUser.username")
+    const otpGenerated = sendEmailOtp(req.params.email, findUser.username)
 
     const sendTo = userSchema.findOneAndUpdate(
         {email: req.params.email},
