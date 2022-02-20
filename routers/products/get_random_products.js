01cae044b353e78d4142bbfb640eb25b0fe034e0
Sayyam <sayyammshah@gmail.com>
2022-02-20 15:44:00 +0530

    aggregate limit changed

--- routers/products/get_random_products.js
+++ routers/products/get_random_products.js
@@ -4,7 +4,7 @@ const authToken = require('../../validations_and_auths/authentication_token')
 
 get_random_product_router.get('/', authToken, async(req,res)=>{
 
-    productSchema.aggregate([], function(err, docs) {
+    productSchema.aggregate([{$sample : {size:1000}}], function(err, docs) {
         res.json(docs)
 });
 
