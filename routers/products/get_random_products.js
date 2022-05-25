65b43eb872f87a00a3616fdacda5971569472021
Sayyam Shah <77239396+Sayyamshah9@users.noreply.github.com>
2022-05-25 15:48:49 +0530

    Update get_random_products.js
    Authentication removed from getrandom products route

--- routers/products/get_random_products.js
+++ routers/products/get_random_products.js
@@ -2,7 +2,7 @@ const get_random_product_router = require('express').Router()
 const productSchema = require('../../dbschemas/productschema')
 const authToken = require('../../validations_and_auths/authentication_token')
 
-get_random_product_router.get('/', authToken, async(req,res)=>{
+get_random_product_router.get('/', async(req,res)=>{
 
     productSchema.aggregate([
         {$match: {pverify: true}},
