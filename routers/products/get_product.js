086a951d94eff90b90b2f6bba2777d832dca7aec
Sayyam Shah <77239396+Sayyamshah9@users.noreply.github.com>
2022-05-29 23:15:01 +0530

    Update get_product.js
    Get Product Router Modified

--- routers/products/get_product.js
+++ routers/products/get_product.js
@@ -4,11 +4,7 @@ const productSchema = require('../../dbschemas/productschema')
 get_product_router.get('/:productId', async(req,res)=>{
 
     const productDetails = await productSchema.findById(req.params.productId)
-    if(productDetails != 0){
-        res.status(200).json(productDetails)
-    }else{
-        res.status(400).json({msg:"not Found"})
-    }
+    res.status(200).json(productDetails)
 
 })
 
